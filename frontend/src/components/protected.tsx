6c601d2f8b93ae74fbe94097802e91763bd922aa
Zeyad Alagamy <wezza2003@gmail.com>
2024-05-10 03:06:09 +0300

    Replace useLogin hook with useUser hook in ProtectedRoute component

--- frontend/src/components/protected.tsx
+++ frontend/src/components/protected.tsx
@@ -1,11 +1,22 @@
-import { Navigate } from "react-router-dom";
-import useLogin from "../hooks/useLogin";
+import useUser from "../hooks/useUser";
 import Loading from "../components/loading";
+import { Navigate, useNavigate, useLocation } from "react-router-dom";
+
+const publicRoutes = ["/login"];
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const isLoggedIn = useLogin();
-  if (isLoggedIn === undefined) return <Loading />;
-  return isLoggedIn ? children : <Navigate replace to="/login" />;
+  const user = useUser();
+  if (user === undefined) return <Loading />;
+  if (user === null) return <Navigate replace to="/login" />;
+
+  // Redirect to home if user is already logged in
+  const { pathname } = useLocation();
+  if (publicRoutes.includes(pathname)) {
+    const navigate = useNavigate();
+    navigate("/");
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
---
+++ frontend/src/hooks/useUser.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { signInObserver } from "../firebase/auth";
+import type { User } from "firebase/auth";
+
+const useUser = () => {
+  const [user, setUser] = useState<User | null>();
+
+  useEffect(() => {
+    signInObserver((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return user;
+};
+
+export default useUser;
