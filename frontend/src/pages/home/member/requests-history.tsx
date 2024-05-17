6343d47e6cf9377fad6de6f681f1d1f128071a13
Zeyad Alagamy <wezza2003@gmail.com>
2024-05-17 03:26:11 +0300

    Remove dummy data and handle loading and error states in RequestsHistory component

--- frontend/src/pages/home/member/requests-history.tsx
+++ frontend/src/pages/home/member/requests-history.tsx
@@ -27,49 +27,25 @@ const StatusChip = ({ status }: { status: string }) => {
   );
 };
 
-const DUMMY_REQUESTS = [
-  {
-    id: "1",
-    group: "groups/00gjdgxs1rpa79k",
-    description: "Request 1",
-    status: "pending",
-    created_at: "2021-10-01T00:00:00Z",
-  },
-  {
-    id: "2",
-    group: "groups/04anzqyu2rf1op7",
-    description: "Request 2",
-    status: "approved",
-    created_at: "2021-10-02T00:00:00Z",
-  },
-  {
-    id: "3",
-    group: "groups/041mghml2fe4tkz",
-    description: "Request 3",
-    status: "rejected",
-    created_at: "2021-10-03T00:00:00Z",
-  },
-];
-
 export const RequestsHistory = () => {
-  // const { data: requests, loading } = useFetchQuery(getRequests);
+  const { data: requests, loading } = useFetchQuery(getRequests);
 
   const getGroupName = (groupId: string) => {
     return Object.entries(GROUPS_IDS).find(
       ([, value]) => value === groupId
     )?.[0];
   };
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  // if (requests instanceof Error) {
-  //   return <div>Error: {requests.message}</div>;
-  // }
+  if (requests instanceof Error) {
+    return <div>Error: {requests.message}</div>;
+  }
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-      {DUMMY_REQUESTS.map((request) => (
+      {requests.map((request) => (
         <div
           key={request.id}
           className="flex flex-col gap-2 border rounded shadow p-5 bg-white"
